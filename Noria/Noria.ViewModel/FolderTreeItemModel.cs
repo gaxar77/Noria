fd6e77c9dc50f1a1bd93d9b0cfc1023db5965a52
g.arbia777 <g.arbia777@gmail.com>
2020-10-10 19:46:11 -0400

    -

---
+++ Noria/Noria.ViewModel/FolderTreeItemModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.IO;
+
+namespace Noria.ViewModel
+{
+    public class FolderTreeItemModel : INotifyPropertyChanged
+    {
+        private string _folderName;
+        private string _folderPath;
+        private ObservableCollection<FolderTreeItemModel> _subFolders
+            = new ObservableCollection<FolderTreeItemModel>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string FolderName
+        {
+            get { return _folderName; }
+
+            set
+            {
+                _folderName = value;
+
+                OnPropertyChanged(nameof(FolderName));
+            }
+        }
+
+        public string FolderPath
+        {
+            get { return _folderPath; }
+
+            set
+            {
+                _folderPath = value;
+
+                OnPropertyChanged(nameof(FolderPath));
+            }
+        }
+
+        public ObservableCollection<FolderTreeItemModel> SubFolders
+        {
+            get { return _subFolders; }
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            var e = new PropertyChangedEventArgs(propertyName);
+
+            PropertyChanged?.Invoke(this, e);
+        }
+
+        public FolderTreeItemModel(string folderPath)
+        {
+            FolderPath = folderPath;
+            FolderName = Path.GetFileName(folderPath);
+
+            if (String.IsNullOrEmpty(FolderName))
+                FolderName = FolderPath;
+        }
+
+        public bool LoadSubFolders()
+        {
+            SubFolders.Clear();
+
+            try
+            {
+                var subFolderPaths = Directory.GetDirectories(FolderPath);
+
+                foreach (string subFolderPath in subFolderPaths)
+                {
+                    var subFolder = new FolderTreeItemModel(subFolderPath);
+
+                    SubFolders.Add(subFolder);
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
--- Noria/Noria.ViewModel/FolderTreeViewModel.cs
+++ Noria/Noria.ViewModel/FolderTreeViewModel.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 using System.Security.RightsManagement;
 using System.Collections.ObjectModel;
-using System.IO;
 
 namespace Noria.ViewModel
 {
@@ -26,83 +24,6 @@ namespace Noria.ViewModel
             var e = new PropertyChangedEventArgs(propertyName);
 
             PropertyChanged?.Invoke(this, e);
-        }
-    }
-
-    public class FolderTreeItemModel : INotifyPropertyChanged
-    {
-        private string _folderName;
-        private string _folderPath;
-        private ObservableCollection<FolderTreeItemModel> _subFolders
-            = new ObservableCollection<FolderTreeItemModel>();
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public string FolderName
-        {
-            get { return _folderName; }
-
-            set
-            {
-                _folderName = value;
-
-                OnPropertyChanged(nameof(FolderName));
-            }
-        }
-
-        public string FolderPath
-        {
-            get { return _folderPath; }
-
-            set
-            {
-                _folderPath = value;
-
-                OnPropertyChanged(nameof(FolderPath));
-            }
-        }
-
-        public ObservableCollection<FolderTreeItemModel> SubFolders
-        {
-            get { return _subFolders; }
-        }
-        private void OnPropertyChanged(string propertyName)
-        {
-            var e = new PropertyChangedEventArgs(propertyName);
-
-            PropertyChanged?.Invoke(this, e);
-        }
-
-        public FolderTreeItemModel(string folderPath)
-        {
-            FolderPath = folderPath;
-            FolderName = Path.GetFileName(folderPath);
-
-            if (String.IsNullOrEmpty(FolderName))
-                FolderName = FolderPath;
-        }
-
-        public bool LoadSubFolders()
-        {
-            SubFolders.Clear();
-
-            try
-            {
-                var subFolderPaths = Directory.GetDirectories(FolderPath);
-
-                foreach (string subFolderPath in subFolderPaths)
-                {
-                    var subFolder = new FolderTreeItemModel(subFolderPath);
-
-                    SubFolders.Add(subFolder);
-                }
-
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
         }
     }
 }
